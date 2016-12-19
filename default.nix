44d221df454a30a91cfea84a7f65cb2f782e50ee
Stewart Mackenzie <setori88@gmail.com>
2016-12-20 02:32:45 +0800

    Problem: need to document what's needed
    Solution: document it

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> { };
 with stdenv.lib;
 
 let
-  allCrates = recurseIntoAttrs (callPackage nix-crates-index/all-carg-packages.nix { });
+  allCrates = recurseIntoAttrs (callPackage ../nix-crates-index/all-carg-packages.nix { });
   normalizeName = builtins.replaceStrings [ "-"] ["_"];
   depsStringCalc = pkgs.lib.fold ( dep: str: "${str} --extern ${normalizeName dep.name}=${dep}/lib${normalizeName dep.name}.rlib") "";
 
@@ -26,11 +26,11 @@ rec {
   nixcrates = stdenv.mkDerivation rec {
     name = "nixcrates";
     src = ./src;
-   
+
     deps = [ allCrates.walkdir allCrates.rustc-serialize allCrates.rustache ];
     crates = depsStringCalc deps;
     crateDeps = cratesDeps [] deps;
-    buildInputs = with allCrates; crateDeps ++ deps; 
+    buildInputs = with allCrates; crateDeps ++ deps;
     buildPhase = ''
       ${symlinkCalc buildInputs}
 #       du -a
@@ -51,7 +51,8 @@ rec {
 
     buildPhase = ''
       ${rustcNightly}/bin/rustc $src/main.rs ${depsString}
-      ./main 
+      ./main
+      
     '';
     installPhase=''
       mkdir $out
@@ -74,7 +75,7 @@ rec {
 # [pid 14162] execve("/nix/store/fff3jbf9vbqhmf6qjrmzhliq516x7yrf-rustc-1.11.0/bin/rustc", ["rustc", "src/main.rs", "--crate-name", "hello_flate2", "--crate-type", "bin", "-g", "--out-dir", "/home/joachim/Desktop/projects/fractalide/fetchUrl/hello_flate2/target/debug", "--emit=dep-info,link", "-L", "dependency=/home/joachim/Desktop/projects/fractalide/fetchUrl/hello_flate2/target/debug", "-L", "dependency=/home/joachim/Desktop/projects/fractalide/fetchUrl/hello_flate2/target/debug/deps", "--extern", "flate2=/home/joachim/Desktop/projects/fractalide/fetchUrl/hello_flate2/target/debug/deps/libflate2-d719035eaa7c6a88.rlib", "-L", "native=/home/joachim/Desktop/projects/fractalide/fetchUrl/hello_flate2/target/debug/build/miniz-sys-60c8d67696f63a43/out"], [/* 105 vars */]) = 0
 
       ${rustcNightly}/bin/rustc $src/main.rs --crate-type "bin" --emit=dep-info,link --crate-name main -L dependency=mylibs ${depsString} -L native= #flate2=${allCrates.flate2_0_2_14}/libflate2.rlib
-      ./main 
+      ./main
       exit 1
     '';
   };
@@ -82,19 +83,19 @@ rec {
   tar-example = stdenv.mkDerivation rec {
     name = "tar-example";
     src = ./example/src3;
-    buildInputs = with allCrates; [ tar filetime libc xattr ]; 
+    buildInputs = with allCrates; [ tar filetime libc xattr ];
     buildPhase = ''
       ${symlinkCalc buildInputs}
 
       echo "hu" > file1.txt
       echo "bar" > file2.txt
-      echo "batz" > file3.txt 
+      echo "batz" > file3.txt
 
       ${rustcNightly}/bin/rustc $src/main.rs --crate-type "bin" --emit=dep-info,link --crate-name main -L dependency=mylibs --extern tar=${allCrates.tar}/libtar.rlib
 #       du -a
 #       /run/current-system/sw/bin/ldd ./main
       ./main
-#       du -a 
+#       du -a
       if [ -f foo.tar ]; then
         echo -e "---------\nSUCCESS: tar-example was executed successfully!   \n--------"
       else
@@ -109,7 +110,7 @@ rec {
   inherit allCrates;
 
   allTargets = stdenv.mkDerivation rec {
-    name="allTargets";    
+    name="allTargets";
     version="1";
     buildInputs = with allCrates; [ nixcrates nom capnp regex json tiny_http tar-example getopts-example rustfbp rusqlite ];
     src = ./.;
